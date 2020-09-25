58f1f9f8035c3910f422464fadb0f03cc6ae8b85
pep <jmendez1@xtec.cat>
2020-09-25 18:17:26 +0200

    Initial commit

---
+++ test/javaapplication1/CuentaCorrienteTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pep
+ */
+public class CuentaCorrienteTest {
+    
+    public CuentaCorrienteTest() {
+    }
+    
+    /* Aquest mètode s'executa abans de tot */
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    /* Aquest mètode s'executa al final de tot */
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    /* Aquest mètode s'executa abans de cada test */
+    @Before
+    public void setUp() {
+    }
+    
+    /* Aquest mètode s'executa després de cada test */
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of ingresa method, of class CuentaCorriente.
+     */
+    @Test
+    public void testIngresa() {
+       
+    }
+
+    /**
+     * Test of abona method, of class CuentaCorriente.
+     */
+    @Test
+    public void testAbona() throws Exception {
+      
+    }
+    
+}
