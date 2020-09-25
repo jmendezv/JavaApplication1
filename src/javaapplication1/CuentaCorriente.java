cd87b00e5e99410983e0583e2bf19770897366d0
pep <jmendez1@xtec.cat>
2020-09-25 18:23:53 +0200

    Added CuentaCorriente class

---
+++ src/javaapplication1/CuentaCorriente.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author pep
+ * 
+ * POJO Plain Old Java Object
+ * 
+ */
+public class CuentaCorriente {
+
+    private String titular;
+
+    private double saldo;
+
+    public CuentaCorriente(String titular) {
+        this.titular = titular;
+        this.saldo = 0;
+    }
+
+    public CuentaCorriente(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+////    public String getTitular() {
+////        return titular;
+////    }
+////
+////    public void setTitular(String titular) {
+////        this.titular = titular;
+////    }
+////
+    public double getSaldo() {
+        return saldo;
+    }
+////
+////    public void setSaldo(double saldo) {
+////        this.saldo = saldo;
+////    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return "CuentaCorriente{" + "titular=" + titular + ", saldo=" + saldo + '}';
+    }
+
+    public void ingresa(double ingreso) {
+        saldo += ingreso;
+    }
+
+    public void abona(double abono) throws Exception {
+        if (abono > saldo) {
+            throw new Exception("Saldo insuficiente");
+        }
+        saldo -= abono;
+    }
+
+}
