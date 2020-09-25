441454aecb2592238bef1b34bf0369ad1fb73940
pep <jmendez1@xtec.cat>
2020-09-25 18:24:16 +0200

    Added JavaApplication1 class

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author pep
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CuentaCorriente cc1 = new CuentaCorriente("Pepe", 1000.0);
+        System.out.println(cc1);
+    }
+    
+}
